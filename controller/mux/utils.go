8e571c45801246e08552c4b2cfaa672e8f4aa126
amirhossein-ka <amirhossein.karimi9@gmail.com>
2025-04-11 12:47:37 +0330

    test commit

--- controller/mux/utils.go
+++ controller/mux/utils.go
@@ -5,10 +5,12 @@ import (
 	"net/http"
 )
 
-
 func writeJson(w http.ResponseWriter, code int, data map[string]any) {
-    encoder := json.NewEncoder(w)
-    encoder.SetIndent("", "    ")
-    w.WriteHeader(code)
-    encoder.Encode(data)
+	encoder := json.NewEncoder(w)
+	encoder.SetIndent("", "    ")
+	w.WriteHeader(code)
+	err := encoder.Encode(data)
+	if err != nil {
+		return
+	}
 }
